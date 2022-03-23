f1cb8809b2a37623e26b74c048645d21b2ff82ff
Sean Martin <sean@seanmartinstudio.com>
2022-03-23 11:44:42 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+const moviesRender = movies.map((item) => {
+  return (
+  <div>
+    <h3>Name: {item.title}</h3>
+    <li>Time: {item.time}</li>
+    <ul> Genres:
+      <li>{item.genres}</li>
+    </ul>
+  </div>
+  )
+})
+
+
+  return (
+  <div>
+  <h1>Movies</h1>
+  {moviesRender}
+  </div>
+  
+  )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,44 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+ 
+  <ul>
+    <li>
+    <NavLink
+    to="/"
+    exact
+    >
+      Home
+    </NavLink>
+    </li>
+    <li>
+    <NavLink
+    to="/movies"
+    exact
+    >
+      Movies
+    </NavLink>
+    </li>
+    <li>
+    <NavLink
+    to="/directors"
+    exact
+    >
+      Director
+    </NavLink>
+    </li>
+    <li>
+    <NavLink
+    to="/actors"
+    exact
+    >
+      Actors
+    </NavLink>
+    </li>
+  </ul>
+
+  )
 }
 
 export default NavBar;
